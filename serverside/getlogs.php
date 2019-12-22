229584ec85aabca89afa323df84a8fee2e0a5095
Holger Kibur <hkibur.de@mysummitps.org>
2019-12-22 15:01:34 -0800

    fix foldering

---
+++ serverside/getlogs.php
@@ -0,0 +1,19 @@
+$folder_path = "sensor_logs/";
+$config_path = "sensor_logs/config.txt";
+$config_len = 5;
+
+$data_raw = file_get_contents("php://input");
+$data_len = strlen($data_raw);
+
+if($data_len > 0){
+    if(unpack("Cmanifest", $data_raw, 0) > 0){
+        foreach(glob($folder_path . "*.log") as $log_path){
+            echo substr($log_path, strlen($folder_path)) . "\n";
+        }
+    } else {
+        $log_path = substr($data_raw, 1);
+        if(file_exists($sensor_path . $log_path)){
+            echo file_get_contents($sensor_path . $log_path);
+        }
+    }
+}
